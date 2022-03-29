eff4a7677de9cd08424a357b8352fec3c4287345
Sergey Bernyakovich <sb_aktau@bk.ru>
2022-03-29 10:24:32 +0500

    final development

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,6 +1,48 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
+const auth = require('../middleware/auth.middleware')
+const Comment = require('../models/Comment')
 
-router.post('/signUp', async (req, res) => {})
+router
+	.route('/')
+	.get(auth, async (req, res) => {
+		try {
+			const { orderBy, equalTo } = req.query
+
+			const list = await Comment.find({ [orderBy]: equalTo })
+
+			res.status(200).send(list)
+		} catch (e) {
+			res.status(500).json({ message: 'На сервере произошла ошибка!' })
+		}
+	})
+	.post(auth, async (req, res) => {
+		try {
+			const newComment = await Comment.create({
+				...req.body,
+				userId: req.user._id,
+			})
+
+			res.status(201).send(newComment)
+		} catch (error) {}
+	})
+
+router.delete('/:commentId', auth, async (req, res) => {
+	try {
+		const { commentId } = req.params
+
+		const removedCommend = await Comment.findById(commentId)
+
+		if (removedCommend.userId.toString() === req.user._id) {
+			await removedCommend.remove()
+
+			return res.status(200).send(null)
+		} else {
+			res.status(401).json({ message: 'Unautorized' })
+		}
+	} catch (e) {
+		res.status(500).json({ message: 'На сервере произошла ошибка!' })
+	}
+})
 
 module.exports = router
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,33 @@
 const express = require('express')
+const User = require('../models/User')
 const router = express.Router({ mergeParams: true })
+const auth = require('../middleware/auth.middleware')
 
-router.post('/signUp', async (req, res) => {})
+router.patch('/:userId', auth, async (req, res) => {
+	try {
+		const { userId } = req.params
+
+		if (userId === req.user._id) {
+			const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+				new: true,
+			})
+
+			res.send(updatedUser)
+		} else {
+			res.status(401).json({ message: 'Unautorized' })
+		}
+	} catch (e) {
+		res.status(500).json({ message: 'На сервере произошла ошибка!' })
+	}
+})
+
+router.get('/', auth, async (req, res) => {
+	try {
+		const list = await User.find()
+		res.status(200).send(list)
+	} catch (e) {
+		res.status(500).json({ message: 'На сервере произошла ошибка!' })
+	}
+})
 
 module.exports = router
